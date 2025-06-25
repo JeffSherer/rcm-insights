d8eddfd4a8bea65803281dd785723ef113291faf
Jeffrey Sherer <lil.jeff@live.com>
2025-06-25 12:27:09 -0700

    Refactored notebooks and SQL scripts, cleaned up repo

--- sql/02_kpi_views.sql
+++
@@ -1,9 +0,0 @@
--- Create a monthly summary of claims
-create or replace view v_claims_kpis as
-select
-  date_trunc('month', visit_start_date) as claim_month,
-  count(*) as total_claims
-from visit_occurrence
-where visit_start_date is not null
-group by 1
-order by 1;
--- sql/03_create_denial_flag.sql
+++
@@ -1,5 +0,0 @@
-create or replace table fact_claims_with_denial as
-select
-  v.*,
-  case when total_paid is null or total_paid = 0 then 1 else 0 end as denial_flag
-from visit_occurrence v;
